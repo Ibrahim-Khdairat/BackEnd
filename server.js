e36fc4dd07542a278fd85064ab951eae4043e7a5
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-07-06 16:59:10 +0300

    Lab-08 : Working on API (Full Stack App)

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = express();
 require('dotenv').config();
 const weather = require('./assests/weather.json');
 const cors = require('cors');
+const { default: axios } = require('axios');
 server.use(cors());
 
 
@@ -25,24 +26,48 @@ server.get('/test', (request, response) => {
 
 
 // localhost:3001/weatherinfo?cityName=Seattle
-try{
-    server.get('/weatherinfo', (request, response) => {
-        let selectedCity = weather.find(city => {
-            if (request.query.cityName === city.city_name) { return city }
+
+// -------- WeatherBit ---------//
+
+// https://api.weatherbit.io/v2.0/forecast/daily?city=Amman,NC&key=f0f57934a3ad4bdebb32a5e1325ce251
+
+server.get('/weatherinfo', (request, response) => {
+    let weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${request.query.cityName}&key=${process.env.WeatherBit_Key}`;
+
+    axios.get(weatherBitUrl).then(weatherResponse => {
+        let cityObj = weatherResponse.data.data.map(day => {
+            return (new City(day.valid_date, day.weather.description))
         })
-    
-        // response.status(200).send(selectedCity.data);
-    
-        const cityObj = selectedCity.data.map(day => { 
-            return  new City(day.valid_date, day.weather.description)
-        } )
         response.status(200).send(cityObj)
-    
-    
+    }).catch(error => {
+        response.status(404).send(error)
     })
-} catch {
-    response.status(404).send('Error 404 : You send a wrong request')
-}
+})
+
+
+// -------- WeatherBit ---------//
+
+
+// // localhost:3001/weatherinfo?cityName=Seattle
+// -------------------------------------------
+// try{
+//     server.get('/weatherinfo', (request, response) => {
+//         let selectedCity = weather.find(city => {
+//             if (request.query.cityName === city.city_name) { return city }
+//         })
+
+//         // response.status(200).send(selectedCity.data);
+
+//         const cityObj = selectedCity.data.map(day => { 
+//             return  new City(day.valid_date, day.weather.description)
+//         } )
+//         response.status(200).send(cityObj)
+
+
+//     })
+// } catch {
+//     response.status(404).send('Error 404 : You send a wrong request')
+// }
 
 
 
@@ -52,9 +77,6 @@ server.get('*', (request, response) => {
 })
 
 
-server.listen(PORT, () => {
-    console.log(`Listenng on Port : ${PORT}`);
-})
 
 
 class City {
@@ -63,3 +85,7 @@ class City {
         this.description = description;
     }
 }
+
+server.listen(PORT, () => {
+    console.log(`Listenng on Port : ${PORT}`);
+})
