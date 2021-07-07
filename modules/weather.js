18323beebbef10e5707234f8793be03b7faea7bd
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-07-07 12:53:31 +0300

    Refactoring the code

---
+++ modules/weather.js
@@ -0,0 +1,58 @@
+'use strict'
+const { default: axios } = require('axios');
+
+
+// https://api.weatherbit.io/v2.0/forecast/daily?city=Amman,NC&key=f0f57934a3ad4bdebb32a5e1325ce251
+
+
+
+const weatherHandler = (request, response) => {
+    let weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?city=${request.query.cityName}&key=${process.env.WeatherBit_Key}&days=5`;
+
+
+
+    axios.get(weatherBitUrl).then(weatherResponse => {
+        let cityObj = weatherResponse.data.data.map(day => {
+            return (new City(day.valid_date, day.weather.description))
+        })
+        response.status(200).send(cityObj)
+    }).catch(error => {
+        response.status(404).send(error)
+    })
+
+
+}
+
+class City {
+    constructor(date, description) {
+        this.date = date;
+        this.description = description;
+    }
+}
+
+module.exports = weatherHandler;
+
+
+
+
+
+// // localhost:3001/weatherinfo?cityName=Seattle
+// -------------------------------------------
+// try{
+//     server.get('/weatherinfo', (request, response) => {
+//         let selectedCity = weather.find(city => {
+//             if (request.query.cityName === city.city_name) { return city }
+//         })
+
+//         // response.status(200).send(selectedCity.data);
+
+//         const cityObj = selectedCity.data.map(day => { 
+//             return  new City(day.valid_date, day.weather.description)
+//         } )
+//         response.status(200).send(cityObj)
+
+
+//     })
+// } catch {
+//     response.status(404).send('Error 404 : You send a wrong request')
+// }
