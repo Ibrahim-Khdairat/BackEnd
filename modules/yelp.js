e73e0a6e940d946dcc7112cdc5fd2f1a0bfb9a75
Ibrahim-Khdairat <ibrahimkuderat@gmail.com>
2021-07-10 14:29:25 +0300

    Last Update

--- modules/yelp.js
+++ modules/yelp.js
@@ -11,28 +11,33 @@ const yelpHandler = (request, response) => {
     let sQuery_1 = request.query.cityName;
     let sQuery_2 = request.query.term;
     const ENDPOINT = '/businesses/search'
-  
 
-    let yelpREST = axios.create({
-        baseURL: `https://api.yelp.com/v3/businesses/search?location=${sQuery_1}&term=${sQuery_2}`,
-        headers: {
-            Authorization: `Bearer ${process.env.Yelp_API_Key}`,
-            "Content-type": "application/json",
-        },
-    })
+    if (inMemory[sQuery_1] !== undefined) {
+        console.log('We already have the data');
+        response.status(200).send(inMemory[sQuery_1]);
+    } else {
 
-    yelpREST(ENDPOINT, { params: { key: ENDPOINT } }).then(({ data }) => {
-
-        let yelpObj = data.businesses.map(service => {
-            return (new Yelp(service))
+        let yelpREST = axios.create({
+            baseURL: `https://api.yelp.com/v3/businesses/search?location=${sQuery_1}&term=${sQuery_2}`,
+            headers: {
+                Authorization: `Bearer ${process.env.Yelp_API_Key}`,
+                "Content-type": "application/json",
+            },
         })
 
-        response.status(200).send(yelpObj)
-    }).catch(error =>{
-        response.send(error)
-    })
+        yelpREST(ENDPOINT, { params: { key: ENDPOINT } }).then(({ data }) => {
 
+            let yelpObj = data.businesses.map(service => {
+                return (new Yelp(service))
 
+            })
+            inMemory[sQuery_1] = yelpObj;
+            response.status(200).send(yelpObj)
+        }).catch(error => {
+            response.send(error)
+        })
+
+    }
 
 
 
